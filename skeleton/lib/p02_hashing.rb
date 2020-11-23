9ef96743293ab3e0090edaff3423357e8253d726
Zihao Li <pockyhao518@gmail.com>
2020-11-23 13:52:48 -0500

    p02 unfinished

--- skeleton/lib/p02_hashing.rb
+++ skeleton/lib/p02_hashing.rb
@@ -4,11 +4,24 @@ end
 
 class Array
   def hash
+    string = ''
+    self.each do |ele|
+      id = ele.object_id.to_s
+      string += id
+    end
+    string.to_i
   end
 end
 
 class String
   def hash
+    string = ''
+    self.each_char.with_index do |ele, i|
+      # id = ele.object_id.to_s
+      id = ele.object_id.to_i * i
+      string += id.to_s
+    end
+    string.to_i
   end
 end
 
