7dc8aa9d8fca8fa11c01a817778f8a93f413c0a6
kaizhu94 <zhukai0803@gmail.com>
2020-11-23 12:17:54 -0500

    Complete phase 1

--- skeleton/lib/p01_int_set.rb
+++ skeleton/lib/p01_int_set.rb
@@ -1,19 +1,19 @@
-class MaxIntSet
+class MaxIntSet 
   attr_reader :store
   def initialize(max)
     @store = Array.new(max, false)
   end
 
-  def insert(num)
+  def insert(num) #O(1)
     raise 'Out of bounds' if !is_valid?(num)
     @store[num] = true
   end
 
-  def remove(num)
+  def remove(num) #O(1)
     @store[num] = false
   end
 
-  def include?(num)
+  def include?(num) #O(n)
     @store[num] == true
   end
 
@@ -24,28 +24,36 @@ class MaxIntSet
   end
 
   def validate!(num)
+    if is_valid?(num)
+      @store[num] = true
+    end
   end
 end
 
 
 class IntSet
+
   def initialize(num_buckets = 20)
     @store = Array.new(num_buckets) { Array.new }
   end
 
-  def insert(num)
+  def insert(num)#O(n)
+    @store[num % 20] << num if !include?(num)
   end
 
-  def remove(num)
+  def remove(num)#O(n)
+    @store[num % 20].delete(num)
   end
 
-  def include?(num)
+  def include?(num)#O(n)
+    @store[num % 20].include?(num)
   end
 
   private
 
   def [](num)
     # optional but useful; return the bucket corresponding to `num`
+    @store[num % 20]
   end
 
   def num_buckets
@@ -54,32 +62,55 @@ class IntSet
 end
 
 class ResizingIntSet
-  attr_reader :count
+  attr_accessor :count
 
   def initialize(num_buckets = 20)
     @store = Array.new(num_buckets) { Array.new }
     @count = 0
   end
 
-  def insert(num)
+  def insert(num) O(n) + 1
+    if @count == @store.length
+      resize!
+      @store[num % @store.length] << num
+      @count += 1
+    else
+      if !self.include?(num)
+       @store[num % @store.length] << num 
+       @count += 1
+      end
+    end
   end
 
-  def remove(num)
+  def remove(num) #O(n)
+    if include?(num)
+      @store[num % @store.length].delete(num)
+      @count -= 1
+    end
   end
 
-  def include?(num)
+  def include?(num) #O(n)
+    @store[num % @store.length].include?(num)
   end
 
   private
 
   def [](num)
     # optional but useful; return the bucket corresponding to `num`
+    @store[num % @store.length]
   end
 
   def num_buckets
     @store.length
   end
 
-  def resize!
+  def resize! #O(n)
+    temp_store = Array.new(@store.length * 2) { Array.new }
+    @store.each do |busket|
+      busket.each do |ele|
+        temp_store[ele % temp_store.length] << ele
+      end
+    end
+    @store = temp_store
   end
 end
